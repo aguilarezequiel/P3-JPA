93b60ec88dfeee19cbf4076bfc25c4a46a9c4ad2
Ezequiel <83150188+aguilarezequiel@users.noreply.github.com>
2024-09-05 00:15:38 -0300

    Add files via upload

---
+++ jpaBase-main/src/main/java/entidades/Categoria.java
@@ -0,0 +1,32 @@
+package entidades;
+
+import java.util.*;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "categoria")
+@Getter
+@Setter
+@ToString
+@Builder
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "denominacion")
+    private String denominacion;
+
+    @ManyToMany(mappedBy = "categorias")
+    @Builder.Default
+    private List<Articulo> articulos = new ArrayList<Articulo>();
+}
---
+++ jpaBase-main/src/main/java/entidades/Domicilio.java
@@ -0,0 +1,33 @@
+package entidades;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "domicilio")
+@Getter
+@Setter
+@ToString
+@Builder
+public class Domicilio implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre_calle")
+    private String nombreCalle;
+
+    @Column(name = "numero")
+    private int numero;
+
+    @OneToOne(mappedBy = "domicilio")
+    private Cliente cliente;
+}
